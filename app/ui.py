b2a6d6da6153e4decee5879681be78e996641ed2
Sthiber <sthiber133guevara@gmail.com>
2025-05-08 01:25:56 -0500

    Added the ability to browse through user file system and let them select which directory to clean.

--- app/ui.py
+++ app/ui.py
@@ -20,9 +20,16 @@ class FileCleanerApp:
 
         self.browse_directory_button = tk.Button(root, text="Browse", command=self.browse_directory)
         self.browse_directory_button.pack(pady=5)
+
+        self.selected_directory = ""
+
+        
     
-    def browse_directory():
-        pass
+    def browse_directory(self):
+        directory_path = filedialog.askdirectory()
+        if directory_path:
+            self.selected_directory = directory_path
+            print('The selected directory is: ', directory_path)
 
 
 
