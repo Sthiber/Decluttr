26033b378618816781ac40d7a72c7ff9a7d0c503
Sthiber <sthiber133guevara@gmail.com>
2025-05-08 01:14:33 -0500

    Initial UI setup

---
+++ app/ui.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import Tk
+from tkinter import filedialog
+
+
+
+class FileCleanerApp:
+
+    def __init__(self, root : Tk):
+        self.root = root
+        self.root.title('Decluttr')
+
+        self.root.geometry("500x500")
+
+        self.welcome_label = tk.Label(root, text="Welcome to Decluttr")
+        self.welcome_label.pack(pady=10)
+
+        self.initial_label = tk.Label(root, text="Please select the directory you want to clean")
+        self.initial_label.pack(pady=10)
+
+        self.browse_directory_button = tk.Button(root, text="Browse", command=self.browse_directory)
+        self.browse_directory_button.pack(pady=5)
+    
+    def browse_directory():
+        pass
+
+
+
+
+
+
+
+
+
+        
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+from app.ui import FileCleanerApp
+
 
 def main():
     root = tk.Tk()
-    # app = FileCleanerApp(root)
+    app = FileCleanerApp(root)
     root.mainloop()
 
 if __name__ == "__main__":
